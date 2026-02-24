cb1dde88b5b6b974e6eef6d24a0a2617931fb728
SOLOMON RAJ <152507212+S0L0RAJ@users.noreply.github.com>
2026-02-24 06:02:08 +0530

    Create recursion.py

---
+++ recursion.py
@@ -0,0 +1,11 @@
+# Factorial using recursion
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+# Fibonacci using recursion
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
